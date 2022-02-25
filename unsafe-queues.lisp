8f110f7a8190005747eb7f0ae2746dbdc41b2375
hxz <herongxian@163.com>
2022-02-25 20:42:21 +0800

    the push-queue slot of unsafe-fast-lifo was renamed to cur-queue

--- unsafe-queues.lisp
+++ unsafe-queues.lisp
@@ -88,9 +88,9 @@ but it's enough in this lib since the car of lst will never be nil."
 ;;; lifo unbound queue
 
 (defstruct (unsafe-fast-lifo (:conc-name unsafe-lifo-))
-  (push-queue nil :type simple-vector)
+  (cur-queue nil :type simple-vector)
   (queue-list nil :type list)
-  (push-queue-len 1000 :type fixnum) ; length of push-queue
+  (cur-queue-len 1000 :type fixnum) ; length of cur-queue
   (enlarge-size 1.5 :type single-float)
   (enlarge-threshold 1.0 :type single-float))
 
@@ -105,8 +105,8 @@ but it's enough in this lib since the car of lst will never be nil."
   (assert (>= enlarge-size 1.0))
   (assert (> init-length 0))
   (make-unsafe-fast-lifo :queue-list (list queue)
-                         :push-queue queue
-                         :push-queue-len init-length
+                         :cur-queue queue
+                         :cur-queue-len init-length
                          :enlarge-size (float enlarge-size)
                          :enlarge-threshold (float enlarge-threshold)))
 
@@ -122,36 +122,36 @@ but it's enough in this lib since the car of lst will never be nil."
 
 (defmethod enqueue (object (queue unsafe-fast-lifo))
   (declare (optimize (speed 3) (safety 0) (debug 0)))
-  (with-slots (push-queue queue-list enlarge-size enlarge-threshold push-queue-len) queue
+  (with-slots (cur-queue queue-list enlarge-size enlarge-threshold cur-queue-len) queue
     (declare (single-float enlarge-size enlarge-threshold))
-    (declare (fixnum push-queue-len))
-    (if (< (the fixnum (cl-speedy-lifo:queue-count push-queue))
-           (* enlarge-threshold push-queue-len))
-        (cl-speedy-lifo:enqueue object push-queue)
+    (declare (fixnum cur-queue-len))
+    (if (< (the fixnum (cl-speedy-lifo:queue-count cur-queue))
+           (* enlarge-threshold cur-queue-len))
+        (cl-speedy-lifo:enqueue object cur-queue)
         (progn
           (when ;;(%singularp queue-list) ; enlarging by add a new queue in the end of queue-list
-              (eq push-queue (car (last queue-list)))
-            (let* ((new-len (the fixnum (truncate (* push-queue-len enlarge-size))))
+              (eq cur-queue (car (last queue-list)))
+            (let* ((new-len (the fixnum (truncate (* cur-queue-len enlarge-size))))
                    (new-queue (cl-speedy-lifo:make-queue new-len)))
               (setf queue-list (nconc queue-list (list new-queue)))))
-          (if (cl-speedy-lifo:queue-full-p push-queue) ; check to switch to the last element of queue-list
-              (progn (setf push-queue (car (last queue-list))
-                           push-queue-len (cl-speedy-lifo:queue-length push-queue))
-                     (cl-speedy-lifo:enqueue object push-queue))
-              (cl-speedy-lifo:enqueue object push-queue))))))
+          (if (cl-speedy-lifo:queue-full-p cur-queue) ; check to switch to the last element of queue-list
+              (progn (setf cur-queue (car (last queue-list))
+                           cur-queue-len (cl-speedy-lifo:queue-length cur-queue))
+                     (cl-speedy-lifo:enqueue object cur-queue))
+              (cl-speedy-lifo:enqueue object cur-queue))))))
 
 (defmethod queue-peek ((queue unsafe-fast-lifo))
   (declare (optimize (speed 3) (safety 0) (debug 0)))
-  (with-slots (push-queue) queue
-    (cl-speedy-lifo:queue-peek push-queue)))
+  (with-slots (-queue) queue
+    (cl-speedy-lifo:queue-peek cur-queue)))
 
 (defmethod dequeue ((queue unsafe-fast-lifo) &key (keep-in-queue-p t) waitp)
   (declare (optimize (speed 3) (safety 0) (debug 0)))
   (declare (ignore waitp))
-  (with-slots (push-queue queue-list) queue
+  (with-slots (cur-queue queue-list) queue
     (declare (list queue-list))
-    (prog1 (cl-speedy-lifo:dequeue push-queue keep-in-queue-p)
-      (when (and (cl-speedy-lifo:queue-empty-p push-queue)
+    (prog1 (cl-speedy-lifo:dequeue cur-queue keep-in-queue-p)
+      (when (and (cl-speedy-lifo:queue-empty-p cur-queue)
                  (null (%singularp queue-list)))
         (setf queue-list (subseq queue-list 0 (1- (length queue-list)))
-              push-queue (car (last queue-list)))))))
+              cur-queue (car (last queue-list)))))))
