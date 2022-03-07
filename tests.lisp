73a259b635d18ca20d0e7436ff0a8ca04a158dc9
hxz <herongxian@163.com>
2022-03-07 22:41:34 +0800

    test list-queue-flush and list-queue-find

--- tests.lisp
+++ tests.lisp
@@ -17,6 +17,7 @@
 (define-test cl-fast-queues-tests)
 
 (define-test speedy-queue :parent cl-fast-queues-tests)
+(define-test list-queue :parent cl-fast-queues-tests)
 
 (define-test unsafe-queues :parent cl-fast-queues-tests)
 (define-test unsafe-fifo :parent unsafe-queues)
@@ -32,6 +33,7 @@
 ;;;; -----------------------------------------------------------
 
 ;;;; speedy-queue.lisp
+
 (define-test speedy-queue-flush :parent speedy-queue
   (dotimes (i *loop-test-times*)
     (let* ((count (1+ (random 10)))
@@ -45,6 +47,37 @@
         (cl-speedy-queue:enqueue 0 queue))
       (finish (cl-speedy-queue:queue-flush queue))
       (is eq t (cl-speedy-queue:queue-empty-p queue)))))
+
+
+;;;; list-queue.lisp
+
+(define-test list-queue-flush :parent list-queue
+  (dotimes (i *loop-test-times*)
+    (let* ((count (1+ (random 10)))
+           (queue (cl-fast-queues::%make-list-queue))
+           (most-enq (random count)))
+      (dotimes (i most-enq)
+        (cl-fast-queues::%list-queue-enqueue 0 queue))
+      (finish (cl-fast-queues::%list-queue-flush queue))
+      (is eq t (cl-fast-queues::%list-queue-empty-p queue))
+      (dotimes (i most-enq)
+        (cl-fast-queues::%list-queue-enqueue 0 queue))
+      (finish (cl-fast-queues::%list-queue-flush queue))
+      (is eq t (cl-fast-queues::%list-queue-empty-p queue)))))
+
+(define-test list-queue-find :parent list-queue
+  (dotimes (i *loop-test-times*)
+    (let* ((count (1+ (random 10)))
+           (queue (cl-fast-queues::%make-list-queue))
+           (most-enq count)
+           (obj-in (random count))
+           (obj-out (+ count (random 10))))
+      (dotimes (i most-enq)
+        (cl-fast-queues::%list-queue-enqueue i queue))
+      (is eq t (cl-fast-queues::%list-queue-find obj-in queue))
+      (is eq nil (cl-fast-queues::%list-queue-find obj-out queue))
+      (is eq nil (cl-fast-queues::%list-queue-find nil queue)))))
+
 
 ;;;; unsafe-queues.lisp
 
