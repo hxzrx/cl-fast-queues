d03c1fb05572f776bd0c6cab63d047af9f5adbaf
hxz <herongxian@163.com>
2022-04-16 16:42:16 +0800

    improve unsafe-fifo and unsafe-lifo

--- unsafe-queues.lisp
+++ unsafe-queues.lisp
@@ -46,6 +46,7 @@ but it's enough in this lib since the car of lst will never be nil."
   (declare (optimize (speed 3) (safety 0) (debug 0)))
   (cl-speedy-queue:queue-empty-p (unsafe-fifo-pop-queue queue)))
 
+#+:ignore
 (defmethod enqueue (object (queue unsafe-fast-fifo))
   (declare (optimize (speed 3) (safety 0) (debug 0)))
   (with-slots (push-queue queue-list) queue
@@ -59,10 +60,25 @@ but it's enough in this lib since the car of lst will never be nil."
         (setf push-queue new-queue)))
     (cl-speedy-queue:enqueue object push-queue)))
 
+(defmethod enqueue (object (queue unsafe-fast-fifo)) ; faster
+  (declare (optimize (speed 3) (safety 0) (debug 0)))
+  (with-slots (push-queue queue-list) queue
+    (declare (list queue-list))
+    (let ((res (cl-speedy-queue:enqueue object push-queue)))
+      (if (eq res #.*overflow-flag*)
+          (let* ((new-len (the fixnum (truncate (* (the fixnum (cl-speedy-queue:queue-length push-queue))
+                                                   #.*enlarge-size*))))
+                 (new-queue (cl-speedy-queue:make-queue new-len)))
+            (%list-queue-enqueue new-queue queue-list)
+            (setf push-queue new-queue)
+            (cl-speedy-queue:enqueue object push-queue))
+          res))))
+
 (defmethod queue-peek ((queue unsafe-fast-fifo))
   (declare (optimize (speed 3) (safety 0) (debug 0)))
   (cl-speedy-queue:queue-peek (unsafe-fifo-pop-queue queue)))
 
+#+:ignore
 (defmethod dequeue ((queue unsafe-fast-fifo) &optional (keep-in-queue-p t))
   (declare (optimize (speed 3) (safety 0) (debug 0)))
   (with-slots (pop-queue queue-list) queue
@@ -71,6 +87,17 @@ but it's enough in this lib since the car of lst will never be nil."
                  (null (%singularp (%list-queue-contents queue-list))))
         (%list-queue-dequeue queue-list)
         (setf pop-queue (%list-queue-peek queue-list))))))
+
+(defmethod dequeue ((queue unsafe-fast-fifo) &optional (keep-in-queue-p t)) ; faster
+  (declare (optimize (speed 3) (safety 0) (debug 0)))
+  (with-slots (pop-queue queue-list) queue
+    (let ((res (cl-speedy-queue:dequeue pop-queue keep-in-queue-p)))
+      (if (and (eq res *underflow-flag*)
+               (null (%singularp (%list-queue-contents queue-list))))
+          (progn (%list-queue-dequeue queue-list)
+                 (setf pop-queue (%list-queue-peek queue-list))
+                 (cl-speedy-queue:dequeue pop-queue keep-in-queue-p))
+          res))))
 
 (defmethod queue-find (item (queue unsafe-fast-fifo) &key (key #'identity) (test #'eql))
   "If `item' has been found in `queue', return the item that has been found, or else return nil.
@@ -133,6 +160,7 @@ and the order of the returned list is the same as queue order. (so that they wil
   (cl-speedy-lifo:queue-empty-p
    (unsafe-lifo-cur-queue queue)))
 
+#+:ignore
 (defmethod enqueue (object (queue unsafe-fast-lifo))
   (declare (optimize (speed 3) (safety 0) (debug 0)))
   (with-slots (cur-queue queue-list) queue
@@ -146,10 +174,25 @@ and the order of the returned list is the same as queue order. (so that they wil
         (setf cur-queue new-queue)))
     (cl-speedy-lifo:enqueue object cur-queue)))
 
+(defmethod enqueue (object (queue unsafe-fast-lifo))
+  (declare (optimize (speed 3) (safety 0) (debug 0)))
+  (with-slots (cur-queue queue-list) queue
+    ;; when push-queue is full, push a new longer queue to queue-list
+    (let ((res (cl-speedy-lifo:enqueue object cur-queue)))
+      (if (eq res #.*overflow-flag*)
+          (let* ((new-len (the fixnum (truncate (* (the fixnum (cl-speedy-lifo:queue-length cur-queue))
+                                                   #.*enlarge-size*))))
+                 (new-queue (cl-speedy-lifo:make-queue new-len)))
+            (push new-queue queue-list)
+            (setf cur-queue new-queue)
+            (cl-speedy-lifo:enqueue object cur-queue))
+          res))))
+
 (defmethod queue-peek ((queue unsafe-fast-lifo))
   (declare (optimize (speed 3) (safety 0) (debug 0)))
   (cl-speedy-lifo:queue-peek (unsafe-lifo-cur-queue queue)))
 
+#+:ignore
 (defmethod dequeue ((queue unsafe-fast-lifo) &optional (keep-in-queue-p t))
   (declare (optimize (speed 3) (safety 0) (debug 0)))
   (with-slots (cur-queue queue-list) queue
@@ -159,6 +202,18 @@ and the order of the returned list is the same as queue order. (so that they wil
                  (null (%singularp queue-list)))
         (setf queue-list (cdr queue-list)
               cur-queue (car  queue-list))))))
+
+(defmethod dequeue ((queue unsafe-fast-lifo) &optional (keep-in-queue-p t))
+  (declare (optimize (speed 3) (safety 0) (debug 0)))
+  (with-slots (cur-queue queue-list) queue
+    (format t "queue: ~d~%" queue)
+    (let ((res (cl-speedy-lifo:dequeue cur-queue keep-in-queue-p)))
+      (if (and (eq res #.*underflow-flag*)
+               (null (%singularp queue-list)))
+          (progn (setf queue-list (cdr queue-list)
+                       cur-queue  (car queue-list))
+                 (cl-speedy-lifo:dequeue cur-queue keep-in-queue-p))
+          res))))
 
 (defmethod queue-find (item (queue unsafe-fast-lifo) &key (key #'identity) (test #'eql))
   "If `item' has been found in `queue', return the item that has been found, or else return nil.
