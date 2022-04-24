2d58c1682e880fba726e18645eaef3785feb7550
hxz <herongxian@163.com>
2022-04-24 17:17:20 +0800

    for exp

--- packages.lisp
+++ packages.lisp
@@ -24,3 +24,25 @@
    :*underflow-flag*
    :*enlarge-size*
    ))
+
+(defpackage :cl-fast-queues-exp
+  (:use :cl)
+  (:nicknames :safe-fastq)
+  (:export
+   :make-safe-fifo
+   :make-safe-lifo
+   :safe-fifo-p
+   :safe-lifo-p
+   :enqueue
+   :dequeue
+   :queue-peek
+   :queue-empty-p
+   :queue-count
+   :queue-find
+   :queue-flush
+   :queue-to-list
+   :list-to-queue
+   :*overflow-flag*
+   :*underflow-flag*
+   :*enlarge-size*
+   ))
